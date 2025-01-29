f2bc30832698d4ce0eb7a459cc60e97e0a5c1a80
MinMin_Tranova <minatran2003@gmail.com>
2025-01-29 11:59:12 +0100

    Minor adjustments for inventory renderer

--- escapeGame/src/main/java/cz/cvut/fel/pjv/view/RenderInventory.java
+++ escapeGame/src/main/java/cz/cvut/fel/pjv/view/RenderInventory.java
@@ -16,6 +16,11 @@ import javafx.stage.Stage;
 public class RenderInventory {
 
     private final GameBoard gameBoard;
+    private final int ROWS = 4;
+    private final int COLUMNS = 5;
+    private final int SQUARE_WIDTH = 100;
+    private final int SQUARE_HEIGHT = 120;
+    private final int PADDING = 10;
 
     public RenderInventory(GameBoard gameBoard) {
         this.gameBoard = gameBoard;
@@ -23,19 +28,42 @@ public class RenderInventory {
 
     public void render(GraphicsContext graphicsContext, Inventory inventory) {
 
-        int rows = 5;
-        int columns = 5;
-        int squareSize = 80;
-        int padding = 20;
+        Image plusImage = new Image("plus.png");
+        double plusX = (graphicsContext.getCanvas().getWidth() - plusImage.getWidth()) / 2;
+        double plusY =  PADDING + 5;
+        graphicsContext.drawImage(plusImage, plusX, plusY);
 
-        for (int row = 0; row <= rows; row++) {
-            int y = row * (squareSize + padding) + padding;
-            graphicsContext.strokeLine(padding, y, padding + columns * (squareSize + padding), y);
-        }
+        for (int row = 0; row < ROWS; row++) {
+            for (int col = 0; col < COLUMNS; col++) {
 
-        for (int col = 0; col <= columns; col++) {
-            int x = col * (squareSize + padding) + padding;
-            graphicsContext.strokeLine(x, padding, x, padding + rows * (squareSize + padding));
+                int x = col * (SQUARE_WIDTH + PADDING) + PADDING;
+                int y = row * (SQUARE_HEIGHT + PADDING) + PADDING + 50;
+                graphicsContext.strokeRect(x, y, SQUARE_WIDTH, SQUARE_HEIGHT);
+
+
+                int index = row * COLUMNS + col;
+
+                if (index < inventory.getItems().size()) {
+                    Item item = inventory.getItems().get(index);
+                    Image image = new Image(inventory.getImageName(item));
+
+                    // Název předmětu
+                    double textX = x + 10;
+                    double textY = y + PADDING + 10;
+                    graphicsContext.fillText(item.getName(), textX, textY);
+
+                    // Obrázek předmětu
+                    double imageX = x + (SQUARE_WIDTH - image.getWidth()) / 2;
+                    double imageY = textY + graphicsContext.getFont().getSize();
+                    graphicsContext.drawImage(image, imageX, imageY);
+
+                    // Množství předmětu
+                    double quantityWidth = graphicsContext.getFont().getSize() * ("Quantity: " + item.getQuantity()).length();
+                    double quantityX = x + (SQUARE_WIDTH - quantityWidth) / 2 + 25;
+                    double quantityY = imageY + image.getHeight() + 20;
+                    graphicsContext.fillText("Quantity: " + item.getQuantity(), quantityX, quantityY);
+                }
+            }
         }
 
     }
@@ -45,7 +73,10 @@ public class RenderInventory {
         stage.initModality(Modality.APPLICATION_MODAL);
         stage.setTitle("Inventory");
 
-        Canvas canvas = new Canvas(gameBoard.getSize() - 100, gameBoard.getSize() - 100);
+        int widthInventory = COLUMNS * (SQUARE_WIDTH + PADDING) + PADDING;
+        int heightInventory = ROWS * (SQUARE_HEIGHT + PADDING) + PADDING + 50;
+
+        Canvas canvas = new Canvas(widthInventory, heightInventory);
         GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
         render(graphicsContext, inventory);
 
